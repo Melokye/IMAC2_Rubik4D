a22f60fba2d7331a7058882385c4ae925adc9272
Oradimi <loganargouse@gmail.com>
2023-12-05 14:01:42 +0100

    organize for 1x1x1x1

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,25 +1,65 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using static UnityEngine.GraphicsBuffer;
 
 public class Ball: MonoBehaviour {
     [SerializeField]
-    private GameObject[] balls;
+    private GameObject[] balls = new GameObject[8];
     [SerializeField]
     private GameObject center;
+    [SerializeField]
+    private int number;
 
-    // Start is called before the first frame update
-    void Start() {
-        
+    private void Start() {
+        /*GameObject[] things = GameObject.FindObjectsOfType<GameObject>();
+        Debug.Log(GameObject.FindObjectsOfType<GameObject>());
+        foreach (GameObject ball in things)
+        {
+            Debug.Log(ball.name);
+            if (ball.name.Contains("1_Left"))
+            {
+                balls[0].Append(ball);
+            }
+            if (ball.name.Contains("2_Right"))
+            {
+                balls[1].Append(ball);
+            }
+            if (ball.name.Contains("3_Up"))
+            {
+                balls[2].Append(ball);
+            }
+            if (ball.name.Contains("4_Down"))
+            {
+                balls[3].Append(ball);
+            }
+            if (ball.name.Contains("5_Front"))
+            {
+                balls[4].Append(ball);
+            }
+            if (ball.name.Contains("6_Back"))
+            {
+                balls[5].Append(ball);
+            }
+            if (ball.name.Contains("7_In"))
+            {
+                balls[6].Append(ball);
+            }
+            if (ball.name.Contains("8_Out"))
+            {
+                Debug.Log("hey bro what's up");
+                balls[7].Append(ball);
+            }
+        }*/
     }
 
     // Update is called once per frame
     void Update() {
         foreach (GameObject ball in balls)
         {
-            ball.transform.RotateAround(center.transform.position, Vector3.up, 20 * Time.deltaTime);
-
+            ball.transform.RotateAround(center.transform.position, Vector3.up, 60 * Time.deltaTime);
         }
     }
 }
