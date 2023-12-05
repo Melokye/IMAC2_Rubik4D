12e865ac05c4191cb23febcb4273c157ef6fdd24
Oradimi <loganargouse@gmail.com>
2023-12-05 11:18:06 +0100

    balls turning around

--- Assets/Scripts/Ball.cs
+++ Assets/Scripts/Ball.cs
@@ -1,18 +1,25 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static UnityEngine.GraphicsBuffer;
 
-public class Ball : MonoBehaviour
-{
+public class Ball: MonoBehaviour {
+    [SerializeField]
+    private GameObject[] balls;
+    [SerializeField]
+    private GameObject center;
+
     // Start is called before the first frame update
-    void Start()
-    {
+    void Start() {
         
     }
 
     // Update is called once per frame
-    void Update()
-    {
-        
+    void Update() {
+        foreach (GameObject ball in balls)
+        {
+            ball.transform.RotateAround(center.transform.position, Vector3.up, 20 * Time.deltaTime);
+
+        }
     }
 }
