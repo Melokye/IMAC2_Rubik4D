32de99dfabf6c3a9f8a82d54b96515be0507a8d1
Mélo <melodiekouy@gmail.com>
2023-12-06 14:36:06 +0100

    insert enum

--- Assets/Scripts/Menu.cs
+++ Assets/Scripts/Menu.cs
@@ -4,27 +4,29 @@ using System.Collections;
 // source : https://docs.unity3d.com/2022.3/Documentation/Manual/gui-Basics.html
 
 public class Menu : MonoBehaviour {
-    private int state = 0;
+    enum MenuType {Main, Credit};
+    private MenuType state = MenuType.Main;
     private int width = 200;
     private int padding = 10;
 
+    
     void OnGUI ()
     {
         // TODO hide the rubik4D ?
-        switch(state){ // TODO enum ?
-            case 0 : 
+        switch(state){
+            case MenuType.Main : 
                 state = mainMenu(); 
                 break;
-            case 1 : 
+            case MenuType.Credit : 
                 state = credit(); 
                 break;
             default : 
-                state = 0;
+                state = MenuType.Main;
                 break;
         }
     }
 
-    int mainMenu(){
+    MenuType mainMenu(){
         newWindow();
     
         // Make the first button. If it is pressed, Application.Loadlevel (1) will be executed
@@ -34,13 +36,13 @@ public class Menu : MonoBehaviour {
         }
     
         if(GUI.Button(new Rect(padding, 90, width - padding * 2, 20), "Credit")) 
-            return 1; // TODO need ajustement ?
+            return MenuType.Credit;
         
         closeWindow();
-        return 0;
+        return MenuType.Main;
     }
 
-    int credit(){
+    MenuType credit(){
         newWindow();
     
         GUI.Box(new Rect(padding, 60, width - padding * 2, 20), "Tutors");
@@ -52,7 +54,7 @@ public class Menu : MonoBehaviour {
             return 0;
 
         closeWindow();
-        return 1;
+        return MenuType.Credit;
     }
 
     void newWindow(){ // TODO param nb button to display ?
