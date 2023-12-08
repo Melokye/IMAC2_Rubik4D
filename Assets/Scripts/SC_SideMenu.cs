436f9740a011460c53cb893ad136d56abfeb304c
Mélo <melodiekouy@gmail.com>
2023-12-08 11:52:18 +0100

    separate the differents window of the menu

---
+++ Assets/Scripts/SC_SideMenu.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SC_SideMenu : MonoBehaviour
+{
+    public void BackMenuButton()
+    {
+        SceneManager.LoadScene("Menu");
+    }
+}
