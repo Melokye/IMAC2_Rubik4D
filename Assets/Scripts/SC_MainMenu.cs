24fd2647a72eab0e9e4d339bc55a927f5d1a72c0
Mélo <melodiekouy@gmail.com>
2023-12-08 13:53:43 +0100

    create button for axe rotation

--- Assets/Scripts/SC_MainMenu.cs
+++ Assets/Scripts/SC_MainMenu.cs
@@ -5,15 +5,6 @@ using UnityEngine.SceneManagement;
 
 public class SC_MainMenu : MonoBehaviour
 {
-    public GameObject MainMenu;
-    // public GameObject CreditsMenu;
-
-    // Start is called before the first frame update
-    // void Start()
-    // {
-    //     MainMenuButton();
-    // }
-
     public void PlayNowButton()
     {
         // Play Now Button has been pressed, here you can initialize your game
@@ -25,13 +16,6 @@ public class SC_MainMenu : MonoBehaviour
         // Show Credits Menu
         SceneManager.LoadScene("Credit");
     }
-
-    // public void MainMenuButton()
-    // {
-    //     // Show Main Menu
-    //     MainMenu.SetActive(true);
-    //     // CreditsMenu.SetActive(false);
-    // }
 
     public void QuitButton()
     {
