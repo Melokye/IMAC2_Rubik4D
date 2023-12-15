b2ca45430befb6071f0b2a2cfe23bc4d8a810e17
Mélo <melodiekouy@gmail.com>
2023-12-15 12:24:49 +0100

    move functions to generate rings

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,8 +9,7 @@ public class GameManager : MonoBehaviour { // == main
             "Right", "Left", "Up", "Down", "Back", "Front", "In", "Out" };
     List<string> _materials = new List<string>() {
             "Red", "Orange", "Blue", "Green", "Yellow", "White", "Purple", "Pink" };
-    static List<string> _circle_materials = new List<string>() {
-            "XY", "XZ", "YZ", "XW", "YW", "ZW" };
+
     // ---
 
     public GameObject puzzle; // TODO delete it?
@@ -30,7 +29,7 @@ public class GameManager : MonoBehaviour { // == main
     [SerializeField]
     private Mesh sphereMesh;
     private float stickerSize = 0.125f;
-    private static float trailWidth = 0.0078125f;
+    
     public float rotationSpeed = 2f;
 
     // to simplify the camera rotation
@@ -157,131 +156,6 @@ public class GameManager : MonoBehaviour { // == main
                 sticker.transform.position = Projection4DTo3D(p.GetSticker(i, j));
             }
         }
-    }
-
-    /// <summary>
-    /// Rotate a certain amount around a rotation plane and create vertices
-    /// // TODO rephrase the doc + rename fn?
-    /// </summary>
-    /// <param name="stickers"></param>
-    /// <param name="sticker"></param>
-    /// <param name="vertices"></param>
-    /// <param name="axis1"></param>
-    /// <param name="axis2"></param>
-    /// <param name="angle"></param>
-    /// <param name="makeVertices"></param>
-    public static Vector4 TraverseAxis(
-        Vector4 stickers, GameObject sticker, 
-        List<Vector3> vertices, 
-        int axis1, int axis2,
-        float angle, bool makeVertices = true) 
-    {
-        sticker.transform.position = Projection4DTo3D(stickers);
-        if (makeVertices) {
-            float vertexX = trailWidth * Mathf.Sin(angle);
-            float vertexY = trailWidth * Mathf.Sin(angle);
-            float vertexZ = trailWidth * Mathf.Cos(angle);
-            vertices.Add(new Vector3(vertexX, vertexY, vertexZ) + sticker.transform.position);
-            vertices.Add(new Vector3(-vertexX, vertexY, -vertexZ) + sticker.transform.position);
-            vertices.Add(new Vector3(0, -vertexY, 0) + sticker.transform.position);
-        }
-
-        // TODO improve reajustement
-        return Geometry.RotationMatrix((Geometry.Axis)axis1, (Geometry.Axis)axis2, angle) * stickers;
-    }
-
-    /// <summary>
-    /// Create circle mesh from vertices
-    /// </summary>
-    /// <param name="vertices"></param>
-    /// <returns></returns>
-    public static Mesh CreateCircleMesh(List<Vector3> vertices) {
-        // TODO into RingsRepresentation.cs
-        Mesh mesh = new Mesh();
-
-        // add vertices
-        mesh.vertices = vertices.ToArray();
-
-        // create uvs
-        Vector2[] uvs = new Vector2[vertices.Count];
-        for (int i = 1; i < vertices.Count; i++) {
-            if (i % 2 == 0)
-                uvs[i] = new Vector2(i / (vertices.Count - 2f), 0);
-            else
-                uvs[i] = new Vector2((i - 1) / (vertices.Count - 2f), 1);
-        }
-        mesh.uv = uvs;
-
-        // create triangles
-        int[] triangles = new int[vertices.Count * 6];
-        int tri = 0;
-        for (int i = 0; i < vertices.Count * 2; i++) {
-            int shift = Mathf.FloorToInt(i / 6) * 3;
-            int j = i - shift;
-            switch (i % 6) {
-                case 0:
-                    triangles[tri] = (j + 0) % vertices.Count;
-                    triangles[tri + 1] = (j + 1) % vertices.Count;
-                    triangles[tri + 2] = (j + 3) % vertices.Count;
-                    break;
-                case 1:
-                    triangles[tri] = (j + 0) % vertices.Count;
-                    triangles[tri + 1] = (j + 3) % vertices.Count;
-                    triangles[tri + 2] = (j + 2) % vertices.Count;
-                    break;
-                case 2:
-                    triangles[tri] = (j - 1) % vertices.Count;
-                    triangles[tri + 1] = (j + 0) % vertices.Count;
-                    triangles[tri + 2] = (j + 2) % vertices.Count;
-                    break;
-                case 3:
-                    triangles[tri] = (j - 1) % vertices.Count;
-                    triangles[tri + 1] = (j + 2) % vertices.Count;
-                    triangles[tri + 2] = (j + 1) % vertices.Count;
-                    break;
-                case 4:
-                    triangles[tri] = (j - 2) % vertices.Count;
-                    triangles[tri + 1] = (j - 4) % vertices.Count;
-                    triangles[tri + 2] = (j + 1) % vertices.Count;
-                    break;
-                case 5:
-                    triangles[tri] = (j - 5) % vertices.Count;
-                    triangles[tri + 1] = (j - 2) % vertices.Count;
-                    triangles[tri + 2] = (j + 0) % vertices.Count;
-                    break;
-                default:
-                    break;
-            }
-            tri += 3;
-        }
-        mesh.triangles = triangles;
-
-        return mesh;
-    }
-
-    /// <summary>
-    /// Create circle from mesh
-    /// </summary>
-    /// <param name="mesh"></param>
-    /// <param name="plane"></param>
-    /// <param name="tempstickerIndex"></param>
-    /// <returns></returns>
-    public static GameObject CreateCircle(Mesh mesh, int plane, int tempstickerIndex) {
-        // TODO into RingsRepresentation.cs
-        // create gameobject
-        GameObject circle = new GameObject();
-        circle.name = _circle_materials[plane] + "_" + tempstickerIndex;
-
-        // add mesh
-        circle.AddComponent<MeshFilter>();
-        circle.GetComponent<MeshFilter>().mesh = mesh;
-
-        // add material
-        Material circleMat = Resources.Load(_circle_materials[plane], typeof(Material)) as Material;
-        circle.AddComponent<MeshRenderer>();
-        circle.GetComponent<Renderer>().material = circleMat;
-
-        return circle;
     }
 
     public IEnumerator RotationHandler() {
--- Assets/Scripts/RingsRepresentation.cs
+++ Assets/Scripts/RingsRepresentation.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 class RingsRepresentation: MonoBehaviour{
+    private static float trailWidth = 0.0078125f;
     RingsRepresentation(){}
 
     /// <summary>
@@ -40,7 +41,7 @@ class RingsRepresentation: MonoBehaviour{
                 switch (j) {
                     // rotation j = 2 and j = 5 => reajust the sticker position to get on the right circle
                     case 2: case 5:
-                        stickerReference = GameManager.TraverseAxis(stickerReference, tempsticker, vertices,
+                        stickerReference = RingsRepresentation.TraverseAxis(stickerReference, tempsticker, vertices,
                             rotationAxes[j].Item1, rotationAxes[j].Item2, 90f, false);
                         break;
                     
@@ -49,11 +50,11 @@ class RingsRepresentation: MonoBehaviour{
                         // TODO part of the code to generate the rings from one plane
                         for (int k = 0; k < 90; k++) {
                             // need a loop to gradually reach 360°
-                            stickerReference = GameManager.TraverseAxis(stickerReference, tempsticker, 
+                            stickerReference = RingsRepresentation.TraverseAxis(stickerReference, tempsticker, 
                                 vertices, rotationAxes[j].Item1, rotationAxes[j].Item2, 4f);
                         }
-                        Mesh circleMesh = GameManager.CreateCircleMesh(vertices);
-                        GameObject circle = GameManager.CreateCircle(circleMesh, matChoice[j], i);
+                        Mesh circleMesh = RingsRepresentation.CreateCircleMesh(vertices);
+                        GameObject circle = RingsRepresentation.CreateCircle(circleMesh, matChoice[j], i);
                         circle.transform.parent = circleContainer.transform;
                         break;
                 }
@@ -61,5 +62,134 @@ class RingsRepresentation: MonoBehaviour{
             Destroy(tempsticker);
         }
         return circleContainer;
+    }
+
+    /// <summary>
+    /// Create circle from mesh
+    /// </summary>
+    /// <param name="mesh"></param>
+    /// <param name="plane"></param>
+    /// <param name="tempstickerIndex"></param>
+    /// <returns></returns>
+    static GameObject CreateCircle(Mesh mesh, int plane, int tempstickerIndex) {
+        List<string> _circle_materials = new List<string>() {
+        "XY", "XZ", "YZ", "XW", "YW", "ZW" }; // TODO attribute? rename
+
+        // create gameobject
+        GameObject circle = new GameObject();
+        circle.name = _circle_materials[plane] + "_" + tempstickerIndex;
+
+        // add mesh
+        circle.AddComponent<MeshFilter>();
+        circle.GetComponent<MeshFilter>().mesh = mesh;
+
+        // add material
+        Material circleMat = Resources.Load(_circle_materials[plane], typeof(Material)) as Material;
+        circle.AddComponent<MeshRenderer>();
+        circle.GetComponent<Renderer>().material = circleMat;
+
+        return circle;
+    }
+
+    
+    /// <summary>
+    /// Create circle mesh from vertices
+    /// </summary>
+    /// // TODO need to split into multiples functions
+    /// <param name="vertices"></param>
+    /// <returns></returns>
+    static Mesh CreateCircleMesh(List<Vector3> vertices) {
+        // TODO into RingsRepresentation.cs
+        Mesh mesh = new Mesh();
+
+        // add vertices
+        mesh.vertices = vertices.ToArray();
+
+        // create uvs
+        Vector2[] uvs = new Vector2[vertices.Count];
+        for (int i = 1; i < vertices.Count; i++) {
+            if (i % 2 == 0)
+                uvs[i] = new Vector2(i / (vertices.Count - 2f), 0);
+            else
+                uvs[i] = new Vector2((i - 1) / (vertices.Count - 2f), 1);
+        }
+        mesh.uv = uvs;
+
+        // create triangles
+        int[] triangles = new int[vertices.Count * 6];
+        int tri = 0;
+        for (int i = 0; i < vertices.Count * 2; i++) {
+            int shift = Mathf.FloorToInt(i / 6) * 3;
+            int j = i - shift;
+            switch (i % 6) {
+                case 0:
+                    triangles[tri] = (j + 0) % vertices.Count;
+                    triangles[tri + 1] = (j + 1) % vertices.Count;
+                    triangles[tri + 2] = (j + 3) % vertices.Count;
+                    break;
+                case 1:
+                    triangles[tri] = (j + 0) % vertices.Count;
+                    triangles[tri + 1] = (j + 3) % vertices.Count;
+                    triangles[tri + 2] = (j + 2) % vertices.Count;
+                    break;
+                case 2:
+                    triangles[tri] = (j - 1) % vertices.Count;
+                    triangles[tri + 1] = (j + 0) % vertices.Count;
+                    triangles[tri + 2] = (j + 2) % vertices.Count;
+                    break;
+                case 3:
+                    triangles[tri] = (j - 1) % vertices.Count;
+                    triangles[tri + 1] = (j + 2) % vertices.Count;
+                    triangles[tri + 2] = (j + 1) % vertices.Count;
+                    break;
+                case 4:
+                    triangles[tri] = (j - 2) % vertices.Count;
+                    triangles[tri + 1] = (j - 4) % vertices.Count;
+                    triangles[tri + 2] = (j + 1) % vertices.Count;
+                    break;
+                case 5:
+                    triangles[tri] = (j - 5) % vertices.Count;
+                    triangles[tri + 1] = (j - 2) % vertices.Count;
+                    triangles[tri + 2] = (j + 0) % vertices.Count;
+                    break;
+                default:
+                    break;
+            }
+            tri += 3;
+        }
+        mesh.triangles = triangles;
+
+        return mesh;
+    }
+
+    /// <summary>
+    /// Rotate a certain amount around a rotation plane and create vertices
+    /// // TODO rephrase the doc + rename fn?
+    /// </summary>
+    /// <param name="stickers"></param>
+    /// <param name="sticker"></param>
+    /// <param name="vertices"></param>
+    /// <param name="axis1"></param>
+    /// <param name="axis2"></param>
+    /// <param name="angle"></param>
+    /// <param name="makeVertices"></param>
+    static Vector4 TraverseAxis(
+        Vector4 stickers, GameObject sticker, 
+        List<Vector3> vertices, 
+        int axis1, int axis2,
+        float angle, bool makeVertices = true) 
+    {
+        sticker.transform.position = GameManager.Projection4DTo3D(stickers);
+        if (makeVertices) {
+            float vertexX = trailWidth * Mathf.Sin(angle);
+            float vertexY = trailWidth * Mathf.Sin(angle);
+            float vertexZ = trailWidth * Mathf.Cos(angle);
+            vertices.Add(new Vector3(vertexX, vertexY, vertexZ) + sticker.transform.position);
+            vertices.Add(new Vector3(-vertexX, vertexY, -vertexZ) + sticker.transform.position);
+            vertices.Add(new Vector3(0, -vertexY, 0) + sticker.transform.position);
+        }
+
+        // TODO improve reajustement
+        return Geometry.RotationMatrix((Geometry.Axis)axis1, (Geometry.Axis)axis2, angle) * stickers;
     }
 }
