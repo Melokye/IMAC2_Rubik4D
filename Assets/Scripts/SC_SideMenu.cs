ce305b8abe6b227245b5bf7688335d18d6cd6f3c
Mélo <melodiekouy@gmail.com>
2023-12-18 11:48:58 +0100

    add doc

--- Assets/Scripts/SC_SideMenu.cs
+++ Assets/Scripts/SC_SideMenu.cs
@@ -2,11 +2,15 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class SC_SideMenu: MonoBehaviour {
+
+    /// <summary>
+    /// Go back to the main menu
+    /// </summary>
     public void BackMenuButton() {
         SceneManager.LoadScene("Menu");
     }
 
-    // TODO ?
+    // TODO disable or not the command board option
     // public void CommandBoardButton() {
     //     SceneManager.LoadScene("CommandBoard");
     // }
